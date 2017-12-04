0a15cbb5d4894e5b3b1a5b057340134afc062820
miralis <an.gredler@hotmail.com>
2017-12-04 20:09:24 +0100

    init

---
+++ src/main/java/com/example/spring5webapp02/Spring5webapp02Application.java
@@ -0,0 +1,12 @@
+package com.example.spring5webapp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webapp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webapp02Application.class, args);
+	}
+}
